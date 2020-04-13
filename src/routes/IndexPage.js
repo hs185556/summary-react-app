5a55eb0a0707f529db4646984b321e55db87b2dd
hMr <410262593@qq.com>
2020-04-13 15:35:46 +0800

    antd

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import {Steps } from 'antd'
+const { Step } = Steps;
+
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
+      index.html
+      <Steps current={1}>
+        <Step title="Finished" description="This is a description." />
+        <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
+        <Step title="Waiting" description="This is a description." />
+      </Steps>
     </div>
   );
 }
