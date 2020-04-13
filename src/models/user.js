94683b9c30b6a85aac89b3e96088ffd97a9c6861
hMr <410262593@qq.com>
2020-04-13 18:52:45 +0800

    reduce-define-data  for

---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+export default {
+    namespace:'user',
+    state:{
+        list:[
+            {id:1,name:'hello1',pwd:'123'},
+            {id:2,name:'hello2',pwd:'123'},
+            {id:3,name:'hello3',pwd:'123'},
+            {id:4,name:'hello4',pwd:'123'},
+            {id:5,name:'hello5',pwd:'123'},
+        ]
+    },
+    reducer:{
+
+    },
+    effects:{
+
+    },
+    subscriptions:{
+
+    }
+}
--- src/routes/User.js
+++ src/routes/User.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import styles from './User.css'
+import { connect } from 'dva';
 
-function User(){
+function User({list}){
     return (
         <div className={styles.normal}>
             <h2 className={styles.txt}>user.html</h2>
+            {
+                list.map(
+                    (item)=>(
+                        <p key={item.id}>{item.name}</p>
+                    )
+                )
+            }
         </div>
     )
 }
 
-export default User;
+function mapStateToProps(state){
+    const {list} = state.user;
+    return {
+        list
+    }
+}
+
+export default connect(mapStateToProps)(User);
