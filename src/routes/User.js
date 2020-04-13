29ad543be8227eede8687230db8e6b129ed3d93c
hMr <410262593@qq.com>
2020-04-13 17:45:02 +0800

    react-router less

---
+++ src/routes/User.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './User.css'
+
+function User(){
+    return (
+        <div className={styles.normal}>
+            <h2 className={styles.txt}>user.html</h2>
+        </div>
+    )
+}
+
+export default User;
