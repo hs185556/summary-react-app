f9bb9062134a0e3067ffc495bcd66dbd8e3ca3bf
hMr <410262593@qq.com>
2020-04-14 10:19:50 +0800

    show data

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,33 @@ export default {
         ]
     },
     reducer:{
-
+        create(state,{ payload: { user } }){
+            return [...state,user];
+        },
+        delete(state,{payload:id}){
+            return state.list.filter(
+                (item)=>{
+                    return item.id != id;
+                }
+            );
+        },
+        get(state,{payload:id}){
+            return state.list.filter(
+                (item)=>{
+                    return item.id == id;
+                }
+            );
+        },
+        update(state,{payload:user}){
+            for(let i=0;i<state.user.list.length;i++){
+                let item = state.user.list[i];
+                if(item.id == user.id){
+                    state.user.list[i] = user;
+                    return state.user;
+                }
+            }
+            return state.user;
+        }
     },
     effects:{
 
--- src/routes/User.js
+++ src/routes/User.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import styles from './User.css'
 import { connect } from 'dva';
+import {Table} from 'antd';
 
 function User({list}){
+    const columns = [
+        {
+            title: '用户名',
+            dataIndex: 'name',
+            key: 'name',
+          },
+          {
+            title: '密码',
+            dataIndex: 'pwd',
+            key: 'pwd',
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            key: 'action',
+            render: (text, record) => (
+                <span>
+                    <a style={{ marginRight: 16 }}>Invite {record.name}</a>
+                    <a>Delete</a>
+                </span>
+            ),
+          }
+    ]
     return (
         <div className={styles.normal}>
             <h2 className={styles.txt}>user.html</h2>
-            {
-                list.map(
-                    (item)=>(
-                        <p key={item.id}>{item.name}</p>
-                    )
-                )
-            }
+            <Table dataSource={list} columns={columns} />
         </div>
     )
 }
