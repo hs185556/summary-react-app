9b2414a1369a377845ecd9a4b2122a23a280714d
hMr <410262593@qq.com>
2020-04-17 10:40:59 +0800

    重写curd；pagination

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,4 @@
+import vars from '../variable'
 export default {
     namespace:'user',
     state:{
@@ -7,32 +8,151 @@ export default {
             {id:'3',name:'hello3',pwd:'123'},
             {id:'4',name:'hello4',pwd:'123'},
             {id:'5',name:'hello5',pwd:'123'},
-        ]
+            {id:'6',name:'hello6',pwd:'123'},
+            {id:'7',name:'hello7',pwd:'123'},
+            {id:'8',name:'hello8',pwd:'123'},
+            {id:'9',name:'hello9',pwd:'123'},
+            {id:'10',name:'hello10',pwd:'123'},
+            {id:'11',name:'hello11',pwd:'123'},
+            {id:'12',name:'hello12',pwd:'123'},
+            {id:'13',name:'hello13',pwd:'123'},
+            {id:'14',name:'hello14',pwd:'123'},
+            {id:'15',name:'hello15',pwd:'123'},
+            {id:'16',name:'hello16',pwd:'123'},
+            {id:'17',name:'hello17',pwd:'123'},
+            {id:'18',name:'hello18',pwd:'123'},
+            {id:'19',name:'hello19',pwd:'123'},
+            {id:'20',name:'hello20',pwd:'123'},
+            {id:'21',name:'hello21',pwd:'123'},
+            {id:'22',name:'hello22',pwd:'123'},
+            {id:'23',name:'hello23',pwd:'123'},
+            {id:'24',name:'hello24',pwd:'123'},
+            {id:'25',name:'hello25',pwd:'123'},
+            {id:'26',name:'hello26',pwd:'123'},
+            {id:'27',name:'hello27',pwd:'123'},
+            {id:'28',name:'hello28',pwd:'123'},
+            {id:'29',name:'hello29',pwd:'123'},
+            {id:'30',name:'hello30',pwd:'123'},
+            {id:'31',name:'hello31',pwd:'123'},
+            {id:'32',name:'hello32',pwd:'123'},
+            {id:'33',name:'hello33',pwd:'123'},
+            {id:'34',name:'hello34',pwd:'123'},
+            {id:'35',name:'hello35',pwd:'123'},
+            {id:'36',name:'hello36',pwd:'123'},
+            {id:'37',name:'hello37',pwd:'123'},
+            {id:'38',name:'hello38',pwd:'123'},
+            {id:'39',name:'hello39',pwd:'123'},
+            {id:'40',name:'hello40',pwd:'123'},
+        ],
+        paginationData:{
+            currentPageList:[],
+            total:0,
+            page:1
+        }
     },
     reducers:{
-        create(state,{ payload: { user } }){
-            state.list.push(user)
-            const list = [...state.list];
-            return {...state,list};
-        },
-        remove(state,{payload:id}){
-            let list = state.list.filter(
-                (item)=>{
-                    return item.id != id;
-                }
-            );
-            return {...state,list};
-        },
-        update(state,{payload:{user}}){
-            let list = state.list.map((item)=>{
-                if(item.id == user.id)return user;
-                return item;
-            })
-            return{...state,list}
+        updateState(state,{payload}){
+            /* eslint-disable */
+            /* let tmp = {...state,payload} 
+            debugger */
+            /* eslint-disable */
+            return {
+                ...state,
+                ...payload
+            }
         }
     },
     effects:{
-        
+        *getPagination({payload:{page,size}},{put,select}){
+            const start = (page-1)*size;
+            const end = page*size - 1;
+            //模拟网络请求到数据
+            const _state =  yield select(state=>state) 
+            const currentPageList = _state.user.list.filter((item,index)=>{
+                if(index>=start&&index<=end)return item;
+            })
+            const total = _state.user.list.length;
+            //调用reducer保存数据
+            yield put({
+                type:"updateState",
+                payload:{
+                    paginationData:{
+                        currentPageList,
+                        total,
+                        page
+                    }
+                }
+            })
+            return "这是异步返回的数据";
+        },
+        *createUser({payload:{user}},{put,select}){
+            //调取数据
+            const _state =  yield select(state=>state) 
+            _state.user.list.unshift(user)
+            const list = [..._state.user.list];
+            //存数据
+            yield put({
+                type:'updateState',
+                payload:{
+                    list
+                }
+            })
+            //重新获取数据展示
+            yield put({
+                type:'getPagination',
+                payload:{
+                    page:1,
+                    size:vars.pageSize
+                }
+            })
+        },
+        *removeUser({payload:{id}},{put,select}){
+            //调取数据
+            const _state =  yield select(state=>state) 
+            let list = _state.user.list.filter(
+                (item)=>{
+                    if(item.id != id)return item;
+                }
+            );
+            //存数据
+            yield put({
+                type:'updateState',
+                payload:{
+                    list
+                }
+            })
+            //重新获取数据展示
+            yield put({
+                type:'getPagination',
+                payload:{
+                    page:_state.user.paginationData.page,
+                    size:vars.pageSize
+                }
+            })
+        },
+        *updateUser({payload:{user}},{call,put,select}){ //eslint-disable-line
+            //调取数据
+            const _state =  yield select(state=>state) 
+            let list = _state.user.list.map((item)=>{
+                if(item.id == user.id)return user;
+                return item;
+            })
+            //存数据
+            yield put({
+                type:'updateState',
+                payload:{
+                    list
+                }
+            })
+            //重新获取数据展示
+            yield put({
+                type:'getPagination',
+                payload:{
+                    page:_state.user.paginationData.page,
+                    size:vars.pageSize
+                }
+            })
+        },
     },
     subscriptions:{
     },
--- src/routes/User.js
+++ src/routes/User.js
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './User.css'
 import { connect } from 'dva';
-import {Table,Button} from 'antd';
+import { Table, Button, Pagination } from 'antd';
 import UserModal from '../components/UserModalComponent'
+import vars from '../variable'
 
-function User({dispatch,list}){
-    function handleRemove(id){
+function User({ dispatch, paginationData }) {
+    const pageSize = vars.pageSize;
+    useEffect(() => {
         dispatch({
-            type:'user/remove',
-            payload:id
+            type: "user/getPagination",
+            payload: {
+                page: 1,
+                size: pageSize
+            }
+        }).then((v) => {
+            console.log(v);//eslint-disable-line
         })
-    }
-    function propsEditOnOk(user){
+    }, [])
+    function propsEditOnOk(user) {
         dispatch({
-            type:"user/update",
-            payload:{
+            type: "user/updateUser",
+            payload: {
                 user
             }
         })
     }
-    function propsCreateOnOk(user){
+    function propsCreateOnOk(user) {
         dispatch({
-            type:"user/create",
-            payload:{
+            type: "user/createUser",
+            payload: {
                 user
             }
+        })
+    }
+    function onChangePage(value) {
+        dispatch({
+            type: "user/getPagination",
+            payload: {
+                page: value,
+                size: pageSize
+            }
+        })
+    }
+    function handleRemove(id) {
+        dispatch({
+            type: 'user/removeUser',
+            payload: { id }
         })
     }
     const columns = [
@@ -32,40 +54,47 @@ function User({dispatch,list}){
             title: '用户名',
             dataIndex: 'name',
             key: 'name',
-          },
-          {
+        },
+        {
             title: '密码',
             dataIndex: 'pwd',
             key: 'pwd',
-          },
-          {
+        },
+        {
             title: '操作',
             dataIndex: 'action',
             key: 'action',
             render: (text, record) => (
                 <span>
-                    <a style={{ marginRight: 16 }} onClick={()=>handleRemove(record.id)}>remove</a>
+                    <a style={{ marginRight: 16 }} onClick={() => handleRemove(record.id)}>remove</a>
                     <UserModal user={record} onOk={propsEditOnOk} title="修改用户">
                         <a>edit</a>
                     </UserModal>
                 </span>
             ),
-          }
+        }
     ];
     return (
         <div className={styles.normal}>
             <UserModal onOk={propsCreateOnOk} title="创建用户">
                 <Button type="primary">创建用户</Button>
             </UserModal>
-            <Table dataSource={list} columns={columns} rowKey={record => record.id} />
+            <Table
+                dataSource={paginationData.currentPageList}
+                columns={columns}
+                rowKey={record => record.id}
+                pagination={false}
+            />
+            <Pagination defaultCurrent={1} current={paginationData.page} defaultPageSize={vars.pageSize} total={paginationData.total} onChange={onChangePage} />
         </div>
     )
 }
 
-function mapStateToProps(state){
-    const {list} = state.user;
+function mapStateToProps(state) {
+    const { paginationData } = state.user;
     return {
-        list
+        paginationData,
+        // loading:state.loading
     }
 }
 
